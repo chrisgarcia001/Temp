5e04f064d67395c5e0635beb6f9328cdd00ff929
chrisgarcia001 <chrisgarcia001@gmail.com>
2016-09-06 19:25:14 +0000

    removing superfluous

--- opl-experiments-2.0-FIX/experiments.bat
+++ opl-experiments-2.0-FIX/experiments.bat
@@ -1,9 +1,9 @@
-ruby batch_oplrun.rb single-machine-mp-FIXED-timed.mod data/mm output/mm
-ruby batch_oplrun.rb single-machine-mp-FIXED-timed.mod data/mp output/mp
-ruby batch_oplrun.rb single-machine-mp-FIXED-timed.mod data/pm output/pm
-ruby batch_oplrun.rb single-machine-mp-FIXED-timed.mod data/pp output/pp
+jruby batch_oplrun.rb single-machine-mp-FIXED-timed.mod data/mm output/mm
+jruby batch_oplrun.rb single-machine-mp-FIXED-timed.mod data/mp output/mp
+jruby batch_oplrun.rb single-machine-mp-FIXED-timed.mod data/pm output/pm
+jruby batch_oplrun.rb single-machine-mp-FIXED-timed.mod data/pp output/pp
 
-ruby ilog_output_summarizer.rb output/mm
-ruby ilog_output_summarizer.rb output/mp
-ruby ilog_output_summarizer.rb output/pm
-ruby ilog_output_summarizer.rb output/pp
+jruby ilog_output_summarizer.rb output/mm
+jruby ilog_output_summarizer.rb output/mp
+jruby ilog_output_summarizer.rb output/pm
+jruby ilog_output_summarizer.rb output/pp
