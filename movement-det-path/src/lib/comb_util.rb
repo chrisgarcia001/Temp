37e45c81c47010b9ad3cf7b03dd50828ae8d8f7d
Chris Garcia <chrisgarcia001@gmail.com>
2017-03-31 23:29:42 -0400

    Updating comb lib

--- movement-det-path/src/lib/comb_util.rb
+++ movement-det-path/src/lib/comb_util.rb
@@ -12,7 +12,7 @@ module CombUtil
   
   # Returns a hash of number of counts for each unique item, of form {item => count}.
   def table items
-    h = []
+    h = {}
     items.each do |i|
       h[i] = 0 if !h.has_key? i
       h[i] += 1
@@ -31,19 +31,17 @@ module CombUtil
   def bag_diff a, b
     t1 = table a
     t2 = table b
-    t1.each{|item, count| t1[item] -= t2[item] if t2.has_key? item}
+    t1.each{|item, count| t1[item] -= t2[item] if t2.has_key?(item)}
     detable t1
   end
   
   # Generate every possible partitioning of the items into the specified number of groups.
+  # Outputs: list of partitions = list of sequences, where each item in a sequence
+  #          is of form [item, assigned_group]. Groups range from 0 to num_groups - 1.
   def all_partitions items, num_groups
-    if num_groups == 1
-      return powerset(items).map{|s| [s]}
-    else
-      ps = powerset(items)
-      comp_ps = ps.map{|s| bag_diff(items, s)}
-      
-    end
+    return [[]] if items.empty?
+    rest_partitons = all_partitions(items.last(items.length - 1), num_groups)
+    (0..(num_groups - 1)).to_a.map{|g| rest_partitons.map{|part| [[items.first, g]] + part}}.reduce{|x,y| x + y}
   end
   
 end
--- movement-det-path/test/comb_test.rb
+++ movement-det-path/test/comb_test.rb
@@ -2,7 +2,20 @@ require '../src/lib/comb_util'
 
 include CombUtil
 
+# Test bag_diff
+b1 = [1,1,2,2,3,3]
+b2 = [1,2,2,3,4,5,5]
+p bag_diff b1, b2
+p bag_diff b2, b1
+p bag_diff b2, []
+p bag_diff [], b2
+
 # Test powerset
 n = 10
 p powerset((1..n).to_a)
 
+# Test all_partitions
+n = 6
+pts = all_partitions((1..n).to_a, 3)
+p pts.length
+#pts.each{|pt| p pt}
