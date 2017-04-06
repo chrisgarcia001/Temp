ca1b51a190c9310ac0d3969f1715859cd94c71bf
unknown <chrisgarcia001@gmail.com>
2017-04-06 17:51:10 -0400

    Updating params

--- movement-det-path/src/apps/calibrate.bat
+++ movement-det-path/src/apps/calibrate.bat
@@ -1,2 +1,2 @@
 rem ruby .\ga_runner.rb ..\..\params\calibration-solve\ga-calib.csv
-jruby -J-Xmx4096m .\ga_runner.rb ..\..\params\calibration-solve\ga-calib.csv
+jruby -J-Xmx16384m .\ga_runner.rb ..\..\params\calibration-solve\ga-calib.csv
