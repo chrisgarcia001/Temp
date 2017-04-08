76eb4e12bfef1aeb8a65c93178e5e24678b4355e
Chris Garcia <chrisgarcia001@gmail.com>
2017-04-08 16:06:12 -0400

    Updating calibration

--- movement-det-path/src/apps/calibrate.bat
+++ movement-det-path/src/apps/calibrate.bat
@@ -1,2 +1,5 @@
+rem ruby .\greedy_solver_runner.rb ..\..\params\calibration-solve\greedy-calib.csv
+jruby -J-Xmx16384m .\greedy_solver_runner.rb ..\..\params\calibration-solve\greedy-calib.csv
+
 rem ruby .\ga_runner.rb ..\..\params\calibration-solve\ga-calib.csv
 jruby -J-Xmx16384m .\ga_runner.rb ..\..\params\calibration-solve\ga-calib.csv
