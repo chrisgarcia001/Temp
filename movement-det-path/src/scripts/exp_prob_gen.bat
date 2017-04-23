38d8f398980b48a9ef056d8e1d79ac6b1b8f6798
Chris Garcia <chrisgarcia001@gmail.com>
2017-04-22 22:31:39 -0400

    Adding experiment problems and structure

---
+++ movement-det-path/src/scripts/exp_prob_gen.bat
@@ -0,0 +1,11 @@
+ruby ..\apps\problem_generator.rb ..\..\params\experiment\problem-gen\LB.csv
+ruby ..\apps\problem_generator.rb ..\..\params\experiment\problem-gen\LE.csv
+ruby ..\apps\problem_generator.rb ..\..\params\experiment\problem-gen\LS.csv
+ruby ..\apps\problem_generator.rb ..\..\params\experiment\problem-gen\MB.csv
+ruby ..\apps\problem_generator.rb ..\..\params\experiment\problem-gen\ME.csv
+ruby ..\apps\problem_generator.rb ..\..\params\experiment\problem-gen\MS.csv
+ruby ..\apps\problem_generator.rb ..\..\params\experiment\problem-gen\SB.csv
+ruby ..\apps\problem_generator.rb ..\..\params\experiment\problem-gen\SE.csv
+ruby ..\apps\problem_generator.rb ..\..\params\experiment\problem-gen\SS.csv
+rem copy ..\..\data\experiment\S*.csv ..\..\data\experiment\small
+
