e23c79e045b3bd2300d055e96228690291d4de5e
Chris Garcia <chrisgarcia001@gmail.com>
2017-07-07 23:11:17 -0400

    Updating experiment structure

--- DataGen2/run_experiments.bat
+++ DataGen2/run_experiments.bat
@@ -1,17 +1,41 @@
-python .\src\run_experiment.py .\params\mmmq.csv
-python .\src\run_experiment.py .\params\mmms.csv
-python .\src\run_experiment.py .\params\mmpq.csv
-python .\src\run_experiment.py .\params\mmps.csv
-python .\src\run_experiment.py .\params\mpmq.csv
-python .\src\run_experiment.py .\params\mpms.csv
-python .\src\run_experiment.py .\params\mppq.csv
-python .\src\run_experiment.py .\params\mpps.csv
+mkdir problems
+mkdir problems\base
+mkdir problems\transp
 
-python .\src\run_experiment.py .\params\pmmq.csv
-python .\src\run_experiment.py .\params\pmms.csv
-python .\src\run_experiment.py .\params\pmpq.csv
-python .\src\run_experiment.py .\params\pmps.csv
-python .\src\run_experiment.py .\params\ppmq.csv
-python .\src\run_experiment.py .\params\ppms.csv
-python .\src\run_experiment.py .\params\pppq.csv
-python .\src\run_experiment.py .\params\ppps.csv
+rem python .\src\run_experiment.py .\params\base\mmmq.csv
+rem python .\src\run_experiment.py .\params\base\mmms.csv
+rem python .\src\run_experiment.py .\params\base\mmpq.csv
+rem python .\src\run_experiment.py .\params\base\mmps.csv
+rem python .\src\run_experiment.py .\params\base\mpmq.csv
+rem python .\src\run_experiment.py .\params\base\mpms.csv
+rem python .\src\run_experiment.py .\params\base\mppq.csv
+rem python .\src\run_experiment.py .\params\base\mpps.csv
+
+rem python .\src\run_experiment.py .\params\base\pmmq.csv
+rem python .\src\run_experiment.py .\params\base\pmms.csv
+rem python .\src\run_experiment.py .\params\base\pmpq.csv
+rem python .\src\run_experiment.py .\params\base\pmps.csv
+rem python .\src\run_experiment.py .\params\base\ppmq.csv
+rem python .\src\run_experiment.py .\params\base\ppms.csv
+rem python .\src\run_experiment.py .\params\base\pppq.csv
+rem python .\src\run_experiment.py .\params\base\ppps.csv
+del problems\base\*.dat
+
+python .\src\run_experiment.py .\params\transp\mmmq.csv
+python .\src\run_experiment.py .\params\transp\mmms.csv
+python .\src\run_experiment.py .\params\transp\mmpq.csv
+python .\src\run_experiment.py .\params\transp\mmps.csv
+python .\src\run_experiment.py .\params\transp\mpmq.csv
+python .\src\run_experiment.py .\params\transp\mpms.csv
+python .\src\run_experiment.py .\params\transp\mppq.csv
+python .\src\run_experiment.py .\params\transp\mpps.csv
+
+python .\src\run_experiment.py .\params\transp\pmmq.csv
+python .\src\run_experiment.py .\params\transp\pmms.csv
+python .\src\run_experiment.py .\params\transp\pmpq.csv
+python .\src\run_experiment.py .\params\transp\pmps.csv
+python .\src\run_experiment.py .\params\transp\ppmq.csv
+python .\src\run_experiment.py .\params\transp\ppms.csv
+python .\src\run_experiment.py .\params\transp\pppq.csv
+python .\src\run_experiment.py .\params\transp\ppps.csv
+del problems\base\*.dat
