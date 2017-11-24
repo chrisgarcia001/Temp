9795a147777b5d4e32b0b7e3855c1cc4de61f3c7
chrisgarcia001 <chrisgarcia001@gmail.com>
2017-11-24 14:16:35 +0000

    Deleting intermediary files

--- DataGen3-Core/run_experiments.bat
+++ DataGen3-Core/run_experiments.bat
@@ -1,18 +1,33 @@
 python .\src\run_experiment.py .\params\mmmq.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\mmms.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\mmpq.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\mmps.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\mpmq.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\mpms.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\mppq.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\mpps.csv
+del problems\*.dat
 
 python .\src\run_experiment.py .\params\pmmq.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\pmms.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\pmpq.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\pmps.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\ppmq.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\ppms.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\pppq.csv
+del problems\*.dat
 python .\src\run_experiment.py .\params\ppps.csv
 del problems\*.dat
