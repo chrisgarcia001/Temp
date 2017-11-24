baa82d9c91c1edbf45a85d9b120248d4d0ba81cf
chrisgarcia001 <chrisgarcia001@gmail.com>
2017-11-24 14:13:48 +0000

    Deleting intermediate files

--- DataGen3-Transp/run_experiments.bat
+++ DataGen3-Transp/run_experiments.bat
@@ -3,39 +3,68 @@ mkdir problems\base
 mkdir problems\transp
 
 rem python .\src\run_experiment.py .\params\base\mmmq.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\mmms.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\mmpq.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\mmps.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\mpmq.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\mpms.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\mppq.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\mpps.csv
 
 rem python .\src\run_experiment.py .\params\base\pmmq.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\pmms.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\pmpq.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\pmps.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\ppmq.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\ppms.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\pppq.csv
+del problems\base\*.dat
 rem python .\src\run_experiment.py .\params\base\ppps.csv
 del problems\base\*.dat
 
 python .\src\run_experiment.py .\params\transp\mmmq.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\mmms.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\mmpq.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\mmps.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\mpmq.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\mpms.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\mppq.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\mpps.csv
+del problems\transp\*.dat
 
 python .\src\run_experiment.py .\params\transp\pmmq.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\pmms.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\pmpq.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\pmps.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\ppmq.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\ppms.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\pppq.csv
+del problems\transp\*.dat
 python .\src\run_experiment.py .\params\transp\ppps.csv
-del problems\base\*.dat
+del problems\transp\*.dat
